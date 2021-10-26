483d98c9afdd7517bb54bc79f3bbfea5fff0a3a3
okhan1998 <whwlgns111@gmail.com>
2021-10-27 02:28:04 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 import Customer from './components/Customer';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import { withStyles } from '@mui/styles';
+import Paper from '@mui/material/Paper';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
@@ -32,24 +49,25 @@ const customers = [
 
 class App extends Component {
   render(){
+    const { classes } = this.props;
   return (
-    <div>
-    {
-      customers.map(c => {
-        return (
-          <Customer
-            key = {c.id}
-            image = {c.image}
-            name = {c.name}
-            birthday = {c.birthday}
-            gender = {c.gender}
-            job = {c.job}/>
-        )
-      })
-    }
-    </div>
+    <Paper className = {classes.root}>
+      <Table className = {classes.table}>
+        <TableHead>
+          <TableCell>번호</TableCell>
+          <TableCell>이미지</TableCell>
+          <TableCell>이름</TableCell>
+          <TableCell>생년월일</TableCell>
+          <TableCell>성별</TableCell>
+          <TableCell>직업</TableCell>
+        </TableHead>
+        <TableBody>
+          {customers.map(c => {return (<Customer id = {c.id} image = {c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job}/>)})}
+        </TableBody>
+      </Table>
+    </Paper>
   );
-  }
+  } 
 }
 
-export default App;
+export default withStyles(styles)(App);
